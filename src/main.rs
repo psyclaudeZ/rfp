377c6da46cca817e32f318fa37e8f6b2a15a3097
Bowei Zhang <zbw0517@gmail.com>
2025-07-01 22:22:29 -0700

    following along the book

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use std::io::{self, BufRead, BufReader};
 
 fn main() -> io::Result<()> {
-    let stdin = io::stdin();
-    // TODO: come back to double check the perf for large input
-    let reader = BufReader::new(stdin.lock());
+    let lines = parse_lines();
 
-    for (i, line) in reader.lines().enumerate() {
-        let line = line?;
+    for (i, line) in lines?.iter().enumerate() {
+        let line = line;
         println!("Line {}: {}", i + 1, line);
     }
     Ok(())
+}
+
+fn parse_lines() -> io::Result<Vec<String>> {
+    let stdin = io::stdin();
+    // TODO: come back to double check the perf for large input
+    let reader = BufReader::new(stdin.lock());
+    return reader.lines().collect();
 }
