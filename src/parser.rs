c92fab96079443bbef5da0c8dce2178f8e7424c2
Bowei Zhang <zbw0517@gmail.com>
2025-07-04 22:32:06 -0700

    test: unit tests for the parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,7 @@
 use regex::Regex;
 // use std::path::Path;
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 pub struct MatchResult {
     pub path: String,
     pub line_number: Option<u32>,
@@ -35,5 +35,47 @@ impl FilePathParser {
             }
         }
         None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parser::{FilePathParser, MatchResult};
+
+    #[test]
+    fn can_match_standard_path_no_line() {
+        let parser = FilePathParser::new();
+        assert_eq!(
+            parser.match_line("/abc/def/g.e").unwrap(),
+            MatchResult {
+                path: String::from("/abc/def/g.e"),
+                line_number: None,
+            }
+        );
+        assert_eq!(
+            parser.match_line("abcdefg.e").unwrap(),
+            MatchResult {
+                path: String::from("abcdefg.e"),
+                line_number: None,
+            }
+        );
+    }
+
+    #[test]
+    fn can_match_standard_path_with_line_number() {
+        let parser = FilePathParser::new();
+        assert_eq!(
+            parser.match_line("/abc/def/g.e:123").unwrap(),
+            MatchResult {
+                path: String::from("/abc/def/g.e"),
+                line_number: Some(123),
+            }
+        );
+    }
+
+    #[test]
+    fn cannot_match_line_without_extension() {
+        let parser = FilePathParser::new();
+        assert_eq!(parser.match_line("/abc/def:123"), None);
     }
 }
