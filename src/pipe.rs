c86517e8604eba91648a2ccbacc93b3dc3536d72
Bowei Zhang <zbw0517@gmail.com>
2025-07-21 23:48:32 -0700

    feat(parsing): Check file existence and deduplicate

--- src/pipe.rs
+++ src/pipe.rs
@@ -1,11 +1,14 @@
 use crate::parser::FilePathParser;
 use log::debug;
+use std::collections::HashSet;
 use std::io::{self, BufRead, BufReader};
+use std::path::Path;
 
 pub fn run() -> io::Result<Vec<String>> {
     let lines = process_pipe_input();
     let parser = FilePathParser::new();
     let mut matches = vec![];
+    let mut seen: HashSet<String> = HashSet::new();
 
     for line in lines? {
         if let Some(match_result) = parser.match_line(&line) {
@@ -13,7 +16,11 @@ pub fn run() -> io::Result<Vec<String>> {
                 "Matched: {} on line {:?}",
                 match_result.path, match_result.line_number
             );
-            matches.push(match_result.path);
+            if Path::new(&match_result.path).try_exists().unwrap_or(false)
+                && seen.insert(match_result.path.clone())
+            {
+                matches.push(match_result.path);
+            }
         }
     }
     Ok(matches)
